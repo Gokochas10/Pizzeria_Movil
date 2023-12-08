01ffefb82f14df0938747571524981caff99af31
Joshua Herrera <sebas.herrera3152@gmail.com>
2023-12-08 00:30:24 -0500

    Agregacion vista Confirmar-Orden

--- lib/pages/complete_order.dart
+++ lib/pages/complete_order.dart
@@ -1,25 +1,114 @@
-// BlankPage.dart
 import 'package:flutter/material.dart';
 import 'package:proyecto_restaurante/models/dish.dart';
 import 'package:proyecto_restaurante/models/ordenes.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:feather_icons/feather_icons.dart';
 
 class CompleteOrder extends StatelessWidget {
-  const CompleteOrder({super.key});
+  const CompleteOrder({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Blank Page'),
+        title: Text('Mi Orden', style: GoogleFonts.inter(fontSize: 20.0)),
       ),
-      body: Center(
-        child: Column(
-        children: detalles.where((detalle) => detalle.quantity != 0).map((detalle) {
-          // Encuentra el plato que tiene el mismo ID que el detalle
-          Dish dish = ListOfDishes().firstWhere((dish) => dish.id == detalle.product);
-          return Text('Producto: ${dish.name}, Cantidad: ${detalle.quantity}');
-        }).toList(),
+      body: Stack(
+        children: [
+          // Fondo con la imagen
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('../assets/images/bg_confirmar.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          // Contenido principal
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Confirmar Orden',
+                  style: GoogleFonts.inter(
+                      fontSize: 24.0, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Expanded(
+                child: ListView(
+                  children: detalles
+                      .where((detalle) => detalle.quantity != 0)
+                      .map((detalle) {
+                    Dish dish = ListOfDishes()
+                        .firstWhere((dish) => dish.id == detalle.product);
+
+                    return Card(
+                      elevation: 5.0,
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Producto: ${dish.name}',
+                              style: GoogleFonts.inter(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                            SizedBox(height: 8.0),
+                            Text(
+                              'Cantidad: ${detalle.quantity}',
+                              style: GoogleFonts.inter(
+                                fontSize: 14.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    _mostrarSnackbar(context, 'Datos enviados con éxito');
+                  },
+                  icon: Icon(FeatherIcons.checkCircle, color: Colors.white),
+                  label: Text(
+                    'Confirmar Orden',
+                    style: GoogleFonts.inter(fontSize: 18.0)
+                        .copyWith(color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xFFFF4317),
+                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
+    );
+  }
+
+  void _mostrarSnackbar(BuildContext context, String mensaje) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(mensaje),
+        duration: Duration(seconds: 2),
       ),
     );
   }
--- lib/pages/store.dart
+++ lib/pages/store.dart
@@ -1,37 +1,15 @@
 import 'package:feather_icons/feather_icons.dart';
 import 'package:flutter/material.dart';
-import 'package:proyecto_restaurante/models/ordenes.dart';
 import 'package:proyecto_restaurante/sections/store/custom_hero.dart';
 import 'package:proyecto_restaurante/sections/store/store_dishes.dart';
-import 'package:proyecto_restaurante/services/user_services.dart';
 import '../sections/store/options.dart';
 
 class Store extends StatelessWidget {
-  const Store({super.key});
+  const Store({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    ValueNotifier<int> quantityNotifier = ValueNotifier<int>(0);
     List<String> options1 = ["Cena", "Almuerzos", "Fideos", "Postre", "Pizza"];
-    List<OrderdetailsSet> detallesClone = [];
-    void dropDishes() {
-      List<OrderdetailsSet> detalles2 = [];
-      for (var detalle in detalles) {
-        if (detalle.quantity != 0) {
-          detalles2.add(detalle);
-        }
-      }
-      detallesClone = detalles2;
-    }
-
-    void reload() {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (BuildContext context) => Store(),
-        ),
-      );
-    }
 
     List<String> options2 = [
       "Carnes",
@@ -45,21 +23,11 @@ class Store extends StatelessWidget {
       floatingActionButton: Padding(
         padding: const EdgeInsets.only(bottom: 50),
         child: FloatingActionButton(
-          onPressed: () async {
-            detallesClone = List.from(listDetalles());
-            dropDishes();
-            await UserServices().postMensajes('1', detallesClone);
-            //volver cero la cantidad de todos los productos originales
-            for (var detalle in listDetalles()) {
-              detalle.quantity = 0;
-            }
-            detallesClone = [];
-            reload();
-          },
-          backgroundColor: const Color(0xFFFF4317),
-          child: const Stack(
+          onPressed: () {},
+          backgroundColor: Color(0xFFFF4317),
+          child: Stack(
             children: [
-              SizedBox(
+              Container(
                 height: 35,
                 width: 35,
                 child: Icon(FeatherIcons.checkSquare, color: Colors.white),
@@ -69,8 +37,8 @@ class Store extends StatelessWidget {
                 right: 1,
                 child: CircleAvatar(
                   radius: 8,
-                  backgroundColor: Colors.white,
                   child: Text("4", style: TextStyle(fontSize: 12)),
+                  backgroundColor: Colors.white,
                 ),
               ),
             ],
@@ -80,12 +48,12 @@ class Store extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            const CustomHero(),
-            const SizedBox(height: 16),
+            CustomHero(),
+            SizedBox(height: 16),
             Options(options: options1),
-            const SizedBox(height: 16),
+            SizedBox(height: 16),
             Options(options: options2),
-            const StoreDishes(),
+            StoreDishes(),
           ],
         ),
       ),
