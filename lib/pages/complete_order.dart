7013f62ae4f97bf8f39860891f03227b71cccfef
Henry Cortez <henrystevencortez@yahoo.es>
2023-12-07 22:36:08 -0500

    Se agrego contenido a los detalles de la orden

--- lib/pages/complete_order.dart
+++ lib/pages/complete_order.dart
@@ -1,15 +1,25 @@
 // BlankPage.dart
 import 'package:flutter/material.dart';
+import 'package:proyecto_restaurante/models/dish.dart';
+import 'package:proyecto_restaurante/models/ordenes.dart';
 
 class CompleteOrder extends StatelessWidget {
+  const CompleteOrder({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Blank Page'),
+        title: const Text('Blank Page'),
       ),
       body: Center(
-        child: Text('Contenido de la página en blanco'),
+        child: Column(
+        children: detalles.where((detalle) => detalle.quantity != 0).map((detalle) {
+          // Encuentra el plato que tiene el mismo ID que el detalle
+          Dish dish = ListOfDishes().firstWhere((dish) => dish.id == detalle.product);
+          return Text('Producto: ${dish.name}, Cantidad: ${detalle.quantity}');
+        }).toList(),
+      ),
       ),
     );
   }
